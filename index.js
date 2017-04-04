f1902709e98ca4d78648129839df6f4fe71164e2
kshivanku <kshivanku@gmail.com>
2017-04-04 00:37:09 -0400

    travelling salesperson with lexicographic order

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 var cities = [];
-var totalCities = 10;
+var totalCities = 6;
+var order = [];
+var count = 1;
+var totalPermutations;
 var recDist;
 var bestEver;
 
+
 function setup(){
-  createCanvas(400, 300);
+  createCanvas(400, 600);
   for(var i = 0 ; i < totalCities ; i++){
-    var v = createVector(random(width), random(height));
+    var v = createVector(random(width), random(height/2));
     cities[i] = v;
+    order[i] = i;
   }
-  recDist = calDist(cities);
-  bestEver = cities.slice();
+  recDist = calDist(cities, order);
+  bestEver = order.slice();
+  totalPermutations = factorial(totalCities);
 }
 
 function draw(){
   background(0);
-
   noFill();
-  stroke(255);
+  stroke(255, 100);
   strokeWeight(1);
   beginShape();
-  for(var i = 0 ; i < cities.length ; i++){
-    vertex(cities[i].x, cities[i].y);
+  for(var i = 0 ; i < order.length ; i++){
+    var n = order[i];
+    vertex(cities[n].x, cities[n].y);
   }
   endShape();
 
@@ -29,7 +35,8 @@ function draw(){
   strokeWeight(4);
   beginShape();
   for(var i = 0 ; i < bestEver.length ; i++){
-    vertex(bestEver[i].x, bestEver[i].y);
+    var city = cities[bestEver[i]];
+    vertex(city.x, city.y);
   }
   endShape();
 
@@ -39,29 +46,73 @@ function draw(){
     ellipse(cities[i].x, cities[i].y, 8, 8);
   }
 
-  var i = floor(random(cities.length));
-  var j = floor(random(cities.length));
-  swap(cities, i , j);
-  var d = calDist(cities);
+  var d = calDist(cities, order);
   if(d < recDist){
     recDist = d;
-    bestEver = cities.slice();
-    console.log(recDist);
+    bestEver = order.slice();
   }
+  textSize(32);
+  var percent = 100 * (count / totalPermutations);
+  text(nf(percent, 0, 2) + '% complete', 20, height - 100);
+
+  nextOrder();
 }
 
-//It randomly swaps the array so it may never stumble upon the optimal solution and will never know if it has checked all possibilities
+function calDist(points, order){
+  var sum = 0;
+  for(var i = 0 ; i < order.length - 1 ; i++){
+    var cityA = points[order[i]];
+    var cityB = points[order[i + 1]];
+    var d = dist(cityA.x, cityA.y, cityB.x, cityB.y);
+    sum += d;
+  }
+  return sum;
+}
+
+
+function nextOrder(){
+  //STEP 1
+  var largestI = -1;
+  for (var i = 0 ; i < order.length - 1 ; i++){
+    if(order[i] < order[i + 1]){
+      largestI = i;
+    }
+  }
+  if(largestI == -1) {
+    console.log("finished");
+    noLoop();
+  }
+
+  //STEP2
+  var largestJ = largestI;
+  for(var j = largestI + 1 ; j < order.length ; j++){
+    if(order[largestI] < order[j]){
+      largestJ = j;
+    }
+  }
+
+  //STEP3
+  swap(order, largestI, largestJ);
+
+  //STEP4
+  var subVals = order.splice(largestI + 1);
+  var reverse_subVals = subVals.reverse();
+  order = order.concat(reverse_subVals);
+
+  count++;
+}
+
 function swap(a, i , j){
   var temp = a[i];
   a[i] = a[j];
   a[j] = temp;
 }
 
-function calDist(points){
-  var sum = 0;
-  for(var i = 0 ; i < points.length-1 ; i++){
-    var d = dist(points[i].x, points[i].y, points[i+1].x, points[i+1].y);
-    sum += d;
+function factorial(n){
+  if(n == 1) {
+    return 1;
   }
-  return sum;
+  else{
+    return n * factorial(n - 1);
+  }
 }
